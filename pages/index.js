58416e327332e577098ae364c0f05f1e6ef20e53
William V. A. Pereira <williamvictor400wp@gmail.com>
2025-03-24 15:38:07 +0000

    adiciona a pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function home() {
+return <h1>oi mãe tudo bem como vc, eu criei essa pagina com eu intuito de te trazer sentimentos como orgulho, leveza, alegria, mas orgulho eu sei que vc ja tem do seu filho mas isso eu sei que vc ja tem,emtão para comunicar os outros sentimentos eu vou te desejar uma exelente tarde por que vc merece por ser a melhor mãe do mundo, pedir a Deus que te abençoe e te cure de todo o seu cansaço e te dizer que te amo.</h1>
+}
+
+export default home;
