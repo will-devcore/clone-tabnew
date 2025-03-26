36ea2ffcc90426041602aee4553fc5154ed4ba10
William V. A. Pereira <williamvictor400wp@gmail.com>
2025-03-26 18:38:15 +0000

    modifica a frase novamente para mais emosões

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function home() {
-return <h1>oi mãe tudo bem como vc, eu criei essa pagina com eu intuito de te trazer sentimentos como orgulho, leveza, alegria, mas orgulho eu sei que vc ja tem do seu filho que sou eu, emtão para comunicar os outros sentimentos que eu comentei, eu vou te desejar uma exelente tarde porque vc merece por ser a melhor mãe do mundo, pedir a Deus que te abençoe e te cure de todo o seu cansaço e te dizer que te amo muito ❤💖💕😎, mais que o universo e os grãos de areia, e eu agradeço a Deus por te ter como mãe.</h1>
+return <h1>oi mãe tudo bem como vc, eu criei essa pagina com eu intuito de te trazer sentimentos para vc como orgulho, carinho, amo, leveza, alegria, mas orgulho eu sei que vc ja tem do seu filho que sou eu, emtão para comunicar os outros sentimentos que eu comentei, eu vou te desejar uma exelente tarde porque vc merece por ser a melhor mãe do mundo, pedir a Deus que te abençoe e te cure de todo o seu cansaço e te dizer que te amo muito ❤💖💕😎, mais que o universo e os grãos de areia, e eu agradeço a Deus por te ter como mãe, e como isso dito eu gostaria muito de te pedir uma coisa mostra aquele sorriso lindo que vc tem.</h1>
 }
 
 export default home;
