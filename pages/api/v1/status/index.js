958a6d061e2decb50b701c593c662d84f58c1615
William V. A. Pereira <williamvictor400wp@gmail.com>
2025-07-03 21:43:20 +0000

    `databese.js` e `.env`files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sun;"); //as é o que define o nome da query e o que susede ela é o nome em questão
+  console.log(result.rows); //result é uma var const é .rows e um metodo que determina qual a informação está sendo procurada
   response.status(200).json({ chave: "vc é uma pessoa acima da média" });
 }
 
